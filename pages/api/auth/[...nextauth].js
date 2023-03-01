1ecb0cecbf113a60ef120f90306ddc6cfec44408
Shmuel <s600hho@gmail.com>
2023-03-01 15:53:12 +0200

    second attempt at render deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
-    secret: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    secret: process.env.NEXT_PUBLIC_SECRET,
     theme: {
         colorScheme: "light",
     },
